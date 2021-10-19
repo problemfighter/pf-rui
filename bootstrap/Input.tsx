63671d7f2e6c7df05dcb52e2157dcb4ad2a873ab
hmtmcse <hmtm.cse@gmail.com>
2021-10-19 22:29:46 +0600

    adjust component input

--- bootstrap/Input.tsx
+++ bootstrap/Input.tsx
@@ -1,6 +1,7 @@
 import {PFUIState} from "../system/pfui-common-things";
 import InputSpec, {InputProps} from "../spec/InputSpec";
 import CommonTask from "./common/common-task";
+import CommonUtil from "../spec/common/common-util";
 
 
 interface Props extends InputProps {}
@@ -15,8 +16,11 @@ export default class Input extends InputSpec<Props, State> {
 
     render() {
         const _props = this.props;
-        let klasses = ("form-control" + CommonTask.getSizeClass(_props.viewSize, "-") + (_props.className ? " " + _props.className : "")).trim();
-        return (<input {..._props} className={klasses}/>);
+        let klasses = ("form-control" + CommonTask.getSizeClass(_props.viewSize, " form-control-") + (_props.className ? " " + _props.className : "")).trim();
+        return (<input
+            {...CommonUtil.removePropsItem(_props, ['viewSize'])}
+            className={klasses}
+        />);
     }
 
 }
