61078a834ec6643a59229ba13a1e5de6f6d86216
hmtmcse <hmtm.cse@gmail.com>
2021-10-30 00:47:53 +0600

    added sorting feature on table head

--- bootstrap/table/TableHeadCell.tsx
+++ bootstrap/table/TableHeadCell.tsx
@@ -1,4 +1,4 @@
-import TableHeadCellSpec, {TableHeadCellProps} from "../../spec/table/TableHeadCellSpec";
+import TableHeadCellSpec, {SortDirection, TableHeadCellProps} from "../../spec/table/TableHeadCellSpec";
 import {PFUIState} from "../../system/pfui-common-things";
 import CommonUtil from "../../spec/common/common-util";
 
@@ -7,17 +7,69 @@ interface Props extends TableHeadCellProps {
 }
 
 class State implements PFUIState {
+    sortDirection?: SortDirection
 }
 
 export default class TableHeadCell extends TableHeadCellSpec<Props, State> {
 
     static defaultProps = {}
 
+    state: State = new State();
+
+    constructor(props: Props) {
+        super(props);
+        this.state.sortDirection = this.props.sortDirection
+    }
+
+    getSortingOrder() {
+        const _this = this
+        let order = ""
+        if (_this.props.isSortAble) {
+            switch (_this.state.sortDirection) {
+                case "asc":
+                    _this.state.sortDirection = "asc"
+                    return (<i className="bi bi-arrow-down me-1"></i>)
+                case "desc":
+                    _this.state.sortDirection = "desc"
+                    return (<i className="bi bi-arrow-up me-1"></i>)
+                default:
+                    return (<i className="bi bi-arrow-down-up me-1"></i>)
+            }
+        }
+        return order
+    }
+
+    handleOnClickSort(event: any) {
+        let direction: any = this.state.sortDirection
+        if (direction === "asc") {
+            direction = "desc"
+        } else {
+            direction = "asc"
+        }
+        this.setState({sortDirection: direction})
+        if (this.props.onClickSort) {
+            this.props.onClickSort(event, direction)
+        }
+    }
+
+    getTHAttributes() {
+        let attributes: any = {}
+        if (this.props.isSortAble) {
+            attributes.style = {cursor: "pointer"}
+            attributes.onClick = (event: any) => {
+                this.handleOnClickSort(event)
+            }
+        }
+        return attributes
+    }
 
     render() {
         const _props = this.props;
         return (
-            <th {...CommonUtil.addId(_props)} className={CommonUtil.addClassName(_props, "")}>{_props.children}</th>);
+            <th {...CommonUtil.addId(_props)} className={CommonUtil.addClassName(_props, "")} {...this.getTHAttributes()}>
+                {this.getSortingOrder()} {_props.children}
+            </th>
+        );
     }
 
 }
--- spec/table/TableHeadCellSpec.ts
+++ spec/table/TableHeadCellSpec.ts
@@ -2,9 +2,13 @@ import React from "react";
 import {CommonAttributes} from "../common/common-attrs";
 import PFUIReactComponent from "../../system/pfui-common-things";
 
+export type SortDirection = 'asc' | 'desc'
 
 export interface TableHeadCellProps extends React.HTMLAttributes<HTMLTableCellElement>, CommonAttributes {
     children?: any
+    isSortAble?: boolean
+    sortDirection?: SortDirection
+    onClickSort?: (event: any, sortDirection: SortDirection) => void;
 }
 
 
